34ba28f28c60696fd8f2dafb71eda38df29c78ed
michalwachowicz <wachowiczmichal1@gmail.com>
2024-09-22 23:10:31 +0200

    Create merge sort algorithm

--- recursion/mergeSort.js
+++ recursion/mergeSort.js
@@ -12,3 +12,13 @@ const merge = (arr1, arr2) => {
 
   return [...arr, ...arr1.slice(i), ...arr2.slice(j)];
 };
+
+const mergeSort = (arr) => {
+  if (arr.length <= 1) return arr;
+
+  const mid = parseInt(arr.length / 2);
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
+};
