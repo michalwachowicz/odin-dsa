603d08d22a18ef843ab2fd5a701ea73e1fe03271
michalwachowicz <wachowiczmichal1@gmail.com>
2024-09-22 22:53:02 +0200

    Create recursive fibonacci solution

--- recursion/fibonacci.js
+++ recursion/fibonacci.js
@@ -12,3 +12,14 @@ const fibs = (num) => {
 
   return arr;
 };
+
+const fibsRec = (num) => {
+  if (num < 1) return [];
+  if (num === 1) return [0];
+  if (num === 2) return [0, 1];
+
+  const arr = fibsRec(num - 1);
+  arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+
+  return arr;
+};
