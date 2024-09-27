7736f30204af350b591b7d65ab1bb150657fcf1a
michalwachowicz <wachowiczmichal1@gmail.com>
2024-09-27 20:02:30 +0200

    Encapsulate LinkedList properties and add gettters

--- linkedlists/linkedList.js
+++ linkedlists/linkedList.js
@@ -6,37 +6,53 @@ class Node {
 }
 
 class LinkedList {
+  #head;
+  #tail;
+  #size;
+
   constructor() {
-    this.head = null;
-    this.tail = null;
-    this.size = 0;
+    this.#head = null;
+    this.#tail = null;
+    this.#size = 0;
   }
 
   append(value) {
     const node = new Node(value);
 
-    if (!this.tail) {
-      this.head = node;
-      this.tail = node;
+    if (!this.#tail) {
+      this.#head = node;
+      this.#tail = node;
     } else {
-      this.tail.next = node;
-      this.tail = node;
+      this.#tail.next = node;
+      this.#tail = node;
     }
 
-    this.size += 1;
+    this.#size += 1;
   }
 
   prepend(value) {
     const node = new Node(value);
 
-    if (!this.head) {
-      this.head = node;
-      this.tail = node;
+    if (!this.#head) {
+      this.#head = node;
+      this.#tail = node;
     } else {
-      node.next = this.head;
-      this.head = node;
+      node.next = this.#head;
+      this.#head = node;
     }
 
-    this.size += 1;
+    this.#size += 1;
+  }
+
+  size() {
+    return this.#size;
+  }
+
+  head() {
+    return this.#head;
+  }
+
+  tail() {
+    return this.#tail;
   }
 }
