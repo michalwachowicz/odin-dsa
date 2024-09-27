c2e20a51cbc70e58728a40a1bb5ed33acb9a814c
michalwachowicz <wachowiczmichal1@gmail.com>
2024-09-27 19:52:23 +0200

    Create a LinkedList class with append method

--- linkedlists/linkedList.js
+++ linkedlists/linkedList.js
@@ -4,3 +4,26 @@ class Node {
     this.next = null;
   }
 }
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  append(value) {
+    const node = new Node(value);
+
+    if (!this.tail) {
+      this.head = node;
+      this.tail = node;
+      this.size = 1;
+      return;
+    }
+
+    this.tail.next = node;
+    this.tail = this.tail.next;
+    this.size += 1;
+  }
+}
