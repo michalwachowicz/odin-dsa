1b5783500ea2791de9da3d5b5dc78b2e83400d8a
michalwachowicz <wachowiczmichal1@gmail.com>
2024-09-27 20:00:24 +0200

    Implement linked list prepend method

--- linkedlists/linkedList.js
+++ linkedlists/linkedList.js
@@ -25,4 +25,18 @@ class LinkedList {
 
     this.size += 1;
   }
+
+  prepend(value) {
+    const node = new Node(value);
+
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+
+    this.size += 1;
+  }
 }
