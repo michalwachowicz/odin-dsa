9fd68465d80d643d30757ce2a133ae0f20be3689
michalwachowicz <wachowiczmichal1@gmail.com>
2024-09-28 16:28:27 +0200

    Add buildTree and prettyPrint bst functions

--- bst/balancedBst.js
+++ bst/balancedBst.js
@@ -11,3 +11,28 @@ class Tree {
     this.root = new Node(data);
   }
 }
+
+const buildTree = (arr) => {
+  if (arr.length === 0) return null;
+
+  const mid = parseInt((arr.length - 1) / 2);
+  const root = new Node(arr[mid]);
+
+  root.left = buildTree(arr.slice(0, mid));
+  root.right = buildTree(arr.slice(mid + 1));
+
+  return root;
+};
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
