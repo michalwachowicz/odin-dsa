33a0e8d889e67c9b89addc67278c4f5ae05c7577
michalwachowicz <wachowiczmichal1@gmail.com>
2024-09-28 17:07:29 +0200

    Implement tree insert method

--- bst/balancedBst.js
+++ bst/balancedBst.js
@@ -1,7 +1,21 @@
-const { buildTree, cleanArray } = require("./treeUtils");
+const TreeNode = require("./treeNode");
+const { buildTree, cleanArray, prettyPrint } = require("./treeUtils");
 
 class Tree {
   constructor(arr) {
     this.root = buildTree(cleanArray(arr));
+  }
+
+  insert(value, node = this.root) {
+    if (node === null) return new TreeNode(value);
+    if (node.data === value) return node;
+
+    if (value < node.data) {
+      node.left = this.insert(value, node.left);
+    } else {
+      node.right = this.insert(value, node.right);
+    }
+
+    return node;
   }
 }
